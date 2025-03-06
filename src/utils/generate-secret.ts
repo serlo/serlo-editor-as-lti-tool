48e822e1bcf129dff189c0ef722913eda4cfe538
Lars <lars@serlo.org>
2025-03-06 09:54:26 +0100

    feat: drop invalid mongodb entires after generating a new secret

--- src/utils/generate-secret.ts
+++ src/utils/generate-secret.ts
@@ -1,9 +1,27 @@
+import { execSync } from 'child_process'
 import { randomBytes } from 'crypto'
 import { readFile, writeFile } from 'fs/promises'
+import readline from 'readline/promises'
 
 generateSecretKey()
 
 async function generateSecretKey() {
+  // eslint-disable-next-line no-console
+  console.log(
+    'Warning: This will reset mongodb and invalidate all current user sessions. Continue?'
+  )
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+
+  const userAnswer = await rl.question('Continue? [y/n] ')
+
+  rl.close()
+
+  if (userAnswer.trim() !== 'y') return
+
   // Secret key used for HS256
   // Needs to be min. 256 bits
   // Generates 256 random bits and encodes them as text using base64
@@ -25,12 +43,22 @@ async function generateSecretKey() {
 
   await writeFile('.env', newEnvContent)
 
+  const timeoutInMilliseconds = 10000
+
+  // Drop Mongodb database
+  try {
+    execSync(
+      `mongosh --eval 'use test' --eval 'db.dropDatabase()' ${process.env.MONGODB_URI}`,
+      { timeout: timeoutInMilliseconds }
+    )
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(`Failed to drop MongoDB database. Error: ${error}`)
+    return
+  }
+
   // eslint-disable-next-line no-console
-  console.log(
-    'Added new secret key to .env file. Restart the service to use the new secret key.'
-  )
+  console.log('Added new secret key to .env file and dropped MongoDB database.')
   // eslint-disable-next-line no-console
-  console.log(
-    '⚠ If you encounter issues after restarting the server, try deleting session data in the MongoDB database, which is still signed using the old secret key.'
-  )
+  console.log('Important: Restart the service now to use the new secret key.')
 }
