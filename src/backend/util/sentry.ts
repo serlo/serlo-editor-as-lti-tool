4efec18e4334a0ee5c8d5b4334a3c03b58a059fb
Lars <lars@serlo.org>
2025-04-10 14:48:08 +0200

    feat: specify environment in sentry init

--- src/backend/util/sentry.ts
+++ src/backend/util/sentry.ts
@@ -1,7 +1,9 @@
 import * as Sentry from '@sentry/node'
+import config from '../../utils/config'
 
 Sentry.init({
   dsn: 'https://ce6c45bcf8c8c09692ffa88c6f563837@o115070.ingest.us.sentry.io/4508738398060544',
   // Do not send events when 'local'
   enabled: process.env.ENVIRONMENT !== 'local',
+  environment: config.ENVIRONMENT,
 })
