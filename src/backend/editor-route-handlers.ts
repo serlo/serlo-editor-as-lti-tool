577b531709d39b2eafc82785aed57243493528e6
Lars <lars@serlo.org>
2025-04-28 13:00:41 +0200

    fix: saving to edusharing

--- src/backend/editor-route-handlers.ts
+++ src/backend/editor-route-handlers.ts
@@ -129,17 +129,21 @@ export async function onConnect(
         `Invalid LTI custom claim during launch of Serlo editor. Was: ${JSON.stringify(custom)}`
       )
 
-    const mariadb = await getMariaDb()
+    const entity = await getEntity(resourceLinkId)
+    async function getEntity(resourceLinkId: string) {
+      if (config.IS_EDUSHARING_DEPLOYMENT) {
+        return await edusharingApi.getEntity(idToken, custom)
+      }
+      const mariadb = await getMariaDb()
 
-    const entity = config.IS_EDUSHARING_DEPLOYMENT
-      ? await edusharingApi.getEntity(idToken, custom)
-      : await mariadb.createOrGetEntity({
-          custom,
-          idToken,
-          platform,
-          resourceLinkId,
-          user,
-        })
+      return await mariadb.createOrGetEntity({
+        custom,
+        idToken,
+        platform,
+        resourceLinkId,
+        user,
+      })
+    }
 
     const editorMode = getEditorMode(idToken, custom, isEdusharing)
 
@@ -311,7 +315,7 @@ export async function putEntity(
     const isEdusharing = idToken.iss.includes('edu-sharing')
     if (isEdusharing) {
       edusharingApi
-        .putEntity(contentString, res)
+        .putContent(contentString, res)
         // Do not forward error to express. To the user, a failed save to edu-sharing is still considered successful.
         .catch(() => {})
     }
--- src/backend/edusharing/edusharing-api.ts
+++ src/backend/edusharing/edusharing-api.ts
@@ -3,9 +3,10 @@ import { IdToken } from '../types/idtoken'
 import { getEdusharingInfo } from './get-edusharing-info'
 import jwt from 'jsonwebtoken'
 import { createAndLogError } from '../../utils/logger'
+import type { GetEntityBody } from '../../frontend/types/get-entity-body'
 
 export const edusharingApi = {
-  async getEntity(idToken: IdToken, custom: unknown) {
+  async getEntity(idToken: IdToken, custom: unknown): Promise<GetEntityBody> {
     const {
       appId,
       dataToken,
@@ -39,9 +40,13 @@ export const edusharingApi = {
 
     const stringifiedDocumentState = await edusharingResponse.text()
 
+    // No exising content
+    if (stringifiedDocumentState.length === 0)
+      return { id: nodeId, content: null }
+
     return { id: nodeId, content: stringifiedDocumentState }
   },
-  async putEntity(contentString: string, res: Response) {
+  async putContent(contentString: string, res: Response) {
     const {
       appId,
       dataToken,
